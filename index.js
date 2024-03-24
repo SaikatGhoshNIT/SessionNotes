a99bd968977c7ec708a91b0c32f6d3b8c5f3ca07
SaikatGhoshNIT <saikatghosh5434@gmail.com>
2024-03-24 13:11:26 +0530

    fix: the adding of notes on UI

--- index.js
+++ index.js
@@ -1,23 +1,26 @@
 function addSession(){
     const body = document.body
     const note = document.querySelector("#noteInput")
-    const list = document.querySelector(".list")
+    const list = document.querySelector("#list")
 
     const item = document.createElement("div")
-    const itemText = document.createElement("p")
+    const itemText = document.createElement("h3")
     const removeButton = document.createElement("button")
 
     itemText.innerText = note.value
     removeButton.classList.add("remove")
+   // removeButton.textContent("Remove")
+    removeButton.innerText = "Remove"
+    item.classList.add("item")
 
     item.appendChild(itemText)
     item.appendChild(removeButton)
 
     list.appendChild(item)
 
-    note.value=""
+    // note.value=""
 
-    removeButton.addEventListener("click", ()=>{
-        list.removeChild(item)
-    })
+    // removeButton.addEventListener("click", ()=>{
+    //     list.removeChild(item)
+    // })
 }
